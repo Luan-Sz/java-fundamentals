51d2549954238810f0389eb324a8eef896fcd1a9
Luan-Sz <luan.rafaelnoog@gmail.com>
2026-08-01 17:11:44 -0300

    feat: add multiplication table exercise

--- src/io/github/luansz/javafundamentals/basics/Exercise04MultiplicationTable.java
+++ src/io/github/luansz/javafundamentals/basics/Exercise04MultiplicationTable.java
@@ -1,4 +1,18 @@
 package io.github.luansz.javafundamentals.basics;
 
+import java.util.Scanner;
+
 public class Exercise04MultiplicationTable {
+    static void main() {
+        int num;
+        int count = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        num = scanner.nextInt();
+        do {
+            System.out.println(num + " x " + (count + 1) + " = " + (num * (count + 1)));
+            count++;
+        } while (count < 10);
+        scanner.close();
+    }
 }
