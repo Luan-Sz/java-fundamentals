a6b7d5fcaf9824d4bf88b4e428cda2e59e48543b
Luan-Sz <luan.rafaelnoog@gmail.com>
2026-08-02 18:43:26 -0300

    feat: add service price exercise

--- src/io/github/luansz/javafundamentals/basics/Exercise05ServicePrice.java
+++ src/io/github/luansz/javafundamentals/basics/Exercise05ServicePrice.java
@@ -1,4 +1,30 @@
 package io.github.luansz.javafundamentals.basics;
 
+import java.util.Scanner;
+
 public class Exercise05ServicePrice {
+    static void main() {
+        String serviceName;
+        double ogprice;
+        double newprice;
+        double discountAmount;
+        double discount;
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter the service name: ");
+        serviceName=scanner.nextLine();
+        System.out.print("Enter the service price: ");
+        ogprice=scanner.nextDouble();
+        System.out.print("Enter the discount: ");
+        discount=scanner.nextDouble();
+
+        discountAmount = (ogprice * discount)/100;
+        newprice = (ogprice - discountAmount);
+        System.out.println();
+        System.out.println("Service: " + serviceName);
+        System.out.printf("Original price: $%.2f%n", ogprice);
+        System.out.printf("Discount: %.1f%%%n", discount);
+        System.out.printf("Discount amount: $%.2f%n", discountAmount);
+        System.out.printf("Final price: $%.2f%n", newprice);
+        scanner.close();
+    }
 }
