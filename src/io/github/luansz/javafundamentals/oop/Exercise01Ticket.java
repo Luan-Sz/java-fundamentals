ecee6f91850357be893e95ca083997ba7bae3181
Luan-Sz <luan.rafaelnoog@gmail.com>
2026-08-09 17:09:50 -0300

    feat: add encapsulated ticket oop exercise

---
+++ src/io/github/luansz/javafundamentals/oop/Exercise01Ticket.java
@@ -0,0 +1,15 @@
+package io.github.luansz.javafundamentals.oop;
+
+public class Exercise01Ticket {
+    public static void main(String[] args) {
+        Ticket ticket1 = new Ticket("Login unavailable", 25, 10, true);
+        ticket1.showSummary();
+
+        System.out.println();
+
+        ticket1.setWaitingTime(5);
+        ticket1.setResolutionTime(500);
+        ticket1.setSystemUnavailable(false);
+        ticket1.showSummary();
+    }
+}
---
+++ src/io/github/luansz/javafundamentals/oop/Ticket.java
@@ -0,0 +1,128 @@
+package io.github.luansz.javafundamentals.oop;
+
+public class Ticket {
+    private String ticketTitle;
+    private int waitingTime;
+    private int resolutionTime;
+    private boolean isSystemUnavailable;
+    private String priority;
+    private int sla;
+    private String slaStatus;
+
+    //constructor
+    public Ticket(String ticketTitle, int waitingTime, int resolutionTime, boolean isSystemUnavailable) {
+        this.setTicketTitle(ticketTitle);
+        this.setWaitingTime(waitingTime);
+        this.setResolutionTime(resolutionTime);
+        this.setSystemUnavailable(isSystemUnavailable);
+    }
+
+    //methods
+    public void showSummary() {
+        System.out.println("Ticket Title: " + ticketTitle);
+        System.out.println("Waiting time: " + waitingTime);
+        System.out.println("Resolution time: " + resolutionTime);
+        System.out.println("System Unavailable: " + isSystemUnavailable);
+        System.out.println("Priority:" + priority);
+        System.out.println("SLA: " + sla);
+        System.out.println("SLA Status: " + slaStatus);
+    }
+
+    private void calculatePriority() {
+        if (isSystemUnavailable && waitingTime >= 20) {
+            priority = "CRITICAL";
+        } else if (isSystemUnavailable || waitingTime >= 30) {
+            priority = "HIGH";
+        } else if (waitingTime >= 10) {
+            priority = "MEDIUM";
+        } else {
+            priority = "LOW";
+        }
+    }
+
+    private void calculateSla() {
+        if (priority.equals("CRITICAL")) {
+            sla = 15;
+        } else if (priority.equals("HIGH")) {
+            sla = 30;
+        } else if (priority.equals("MEDIUM")) {
+            sla = 120;
+        } else {
+            sla = 480;
+        }
+    }
+
+    private void calculateSlaStatus() {
+        if (resolutionTime <= sla) {
+            slaStatus = "WITHIN SLA";
+        } else {
+            slaStatus = "SLA BREACHED";
+        }
+    }
+
+    //specials
+    public String getTicketTitle() {
+        return ticketTitle;
+    }
+
+    public void setTicketTitle(String ticketTitle) {
+        if (ticketTitle == null || ticketTitle.isBlank()) {
+            throw new IllegalArgumentException("Ticket title must be placed");
+        } else {
+            this.ticketTitle = ticketTitle;
+        }
+    }
+
+    public int getWaitingTime() {
+        return waitingTime;
+    }
+
+    public void setWaitingTime(int waitingTime) {
+        if (waitingTime <=0){
+            throw new IllegalArgumentException("Waiting time must be greater than zero");
+        } else{
+            this.waitingTime = waitingTime;
+            this.calculatePriority();
+            this.calculateSla();
+            this.calculateSlaStatus();
+        }
+    }
+
+    public int getResolutionTime() {
+        return resolutionTime;
+    }
+
+    public void setResolutionTime(int resolutionTime) {
+        if (resolutionTime <=0){
+            throw new IllegalArgumentException("Resolution time must be greater than zero");
+        } else{
+            this.resolutionTime = resolutionTime;
+            this.calculateSlaStatus();
+        }
+    }
+
+    public boolean isSystemUnavailable() {
+        return isSystemUnavailable;
+    }
+
+    public void setSystemUnavailable(boolean systemUnavailable) {
+        isSystemUnavailable = systemUnavailable;
+        this.calculatePriority();
+        this.calculateSla();
+        this.calculateSlaStatus();
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+
+    public int getSla() {
+        return sla;
+    }
+
+    public String getSlaStatus() {
+        return slaStatus;
+    }
+
+}
